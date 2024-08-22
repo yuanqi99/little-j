5abe1ac28385830eb2328c3bad55e39a703dc127
ShanJu <2304200957@qq.com>
2024-08-22 15:22:39 +0800

    添加 fegin

--- little-module/little-order/src/main/java/com/little/domain/SatOrderTbl.java
+++ little-module/little-order/src/main/java/com/little/domain/SatOrderTbl.java
@@ -32,46 +32,6 @@ public class SatOrderTbl implements Serializable {
 
     private Integer money;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getCommodityCode() {
-        return commodityCode;
-    }
-
-    public void setCommodityCode(String commodityCode) {
-        this.commodityCode = commodityCode;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    public Integer getMoney() {
-        return money;
-    }
-
-    public void setMoney(Integer money) {
-        this.money = money;
-    }
-
     @Override
     public String toString() {
         return "SatOrderTbl{" +
